3be5bd86c0bbb6e48bf309945b7d99c50be2fe04
achahid19 <anasks1999@gmail.com>
2025-05-16 16:28:19 +0100

    secure credentials

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -5,8 +5,9 @@ cd /var/www/html/
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
+
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wp-ks --dbpass=0000 --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=achahid-.42.fr --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar config create --dbname=${DB_DATABASE} --dbuser=${DB_USER} --dbpass=${DB_USER_PASSWORD} --dbhost=${DB_HOST} --allow-root
+./wp-cli.phar core install --url=achahid-.42.fr --title=inception --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
 
 php-fpm7.4 -F
