b03c96b7d8c0668ebd97dd068abd11d37ebcd690
achahid19 <anasks1999@gmail.com>
2025-05-20 18:03:59 +0100

    requiring passwd for root db connection

--- srcs/requirements/mariadb/tools/init.sql
+++ srcs/requirements/mariadb/tools/init.sql
@@ -1,5 +1,9 @@
 -- TODO: Secure hardcoded credentials
 CREATE DATABASE ${DB_DATABASE};
 CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
+-- 'localhost' → user can only connect from the same machine.
+-- '%' → user can connect from any remote IP (wildcard).
