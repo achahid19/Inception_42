d0c612383bec0bf3eeeba8211a5c835e8b63320f
achahid19 <anasks1999@gmail.com>
2025-05-22 05:25:51 +0100

    caching system redis

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,6 +8,9 @@ chmod +x wp-cli.phar
 
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=${DB_DATABASE} --dbuser=${DB_USER} --dbpass=${DB_USER_PASSWORD} --dbhost=${DB_HOST} --allow-root
+./wp-cli.phar config set WP_REDIS_HOST ${CACHE_HOST} --allow-root
+./wp-cli.phar config set WP_REDIS_PORT ${CACHE_PORT} --allow-root
+./wp-cli.phar config set WP_CACHE true --allow-root
 ./wp-cli.phar core install --url=achahid-.42.fr --title=inception --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
 
 php-fpm7.4 -F
