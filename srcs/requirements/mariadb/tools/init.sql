73419cacaedce226f74b76100aa938352171b081
achahid19 <anasks1999@gmail.com>
2025-05-28 14:02:57 +0100

    remove unecessary comments

--- srcs/requirements/mariadb/tools/init.sql
+++ srcs/requirements/mariadb/tools/init.sql
@@ -5,5 +5,5 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
--- 'localhost' → user can only connect from the same machine.
--- '%' → user can connect from any remote IP (wildcard).
+-- 'localhost' -> user can only connect from the same machine.
+-- '%' -> user can connect from any remote IP (wildcard).
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-################################################################
-# script.sh has to be run by ENTRYPOINT from mariadb Dockerfile
-################################################################
-
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
@@ -16,17 +12,9 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 fi
 
 if [ ! -f "/run/mysqld/mysqld.pid" ]; then
-    # sed -i 's/127.0.0.1/mariadb/' /etc/mysql/mariadb.conf.d/50-server.cnf
-    # sed -i 's/user = mysql/user = root/' /etc/mysql/mariadb.conf.d/50-server.cnf
     echo "Inception : ${DB_DATABASE} database is being created."
     envsubst < /etc/mysql/init.sql > /etc/mysql/init_db.sql
 fi
 
 # Start MariaDB
 exec mysqld
-
-# This change should fix the error by:
-
-# Properly initializing the MySQL system tables using mysql_install_db
-# Ensuring proper permissions on the data directory
-# Starting MySQL with the correct user
